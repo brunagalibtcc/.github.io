846480709ba6d129485526cae481501d21fec91f
Helena <higuima13@gmail.com>
2020-10-09 19:37:35 -0300

    adding light

--- js/scene.js
+++ js/scene.js
@@ -1,4 +1,4 @@
 const scene = new THREE.Scene();
 fogColor = new THREE.Color(0x302860);
 scene.background = fogColor;
-scene.fog = new THREE.Fog(fogColor, 20, 150);
+// scene.fog = new THREE.Fog(fogColor, 20, 150);
